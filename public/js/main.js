92d62c3a45a58d70387b8823556821f213a05976
init-zero2023 <santosh.sk11112222@gmail.com>
2021-10-21 00:54:35 +0530

    main.js added to fronted

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('myform')
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    // console.log(e.target.state)
+    // console.log()
+    let state = form.elements['commodity'].value
+    let 
+})
